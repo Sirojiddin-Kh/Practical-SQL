e503825f68b5ea64c9195674bbe4ed9218a3f3d0
Sirojiddin-Kh <sirojiddinhamidov2001@gmail.com>
2021-08-12 06:37:33 +0500

    Working with joins

--- PostgreSqlTutorial.com/Section 3/joins.sql
+++ PostgreSqlTutorial.com/Section 3/joins.sql
@@ -1,1 +1,31 @@
-joins.sql
+create table basket_a (
+	a serial not null primary key,
+	fruit_a varchar(24)
+);
+
+create table basket_b (
+	b serial not null primary key,
+	fruit_b varchar(24)
+);
+
+insert into basket_a (fruit_a)
+	values 
+		('Apple'), ('Orange'), ('Banana'), ('Cucumber');
+
+
+insert into basket_b (fruit_b)
+	values
+		('Orange'), ('Apple'), ('Watermelon'), ('Pear');
+
+
+
+select
+	a, 
+	fruit_a,
+	b,
+	fruit_b
+
+from 
+	basket_a		
+inner join basket_b
+	on fruit_a = fruit_b;
